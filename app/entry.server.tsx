01ad17b9c680f22ed8eccd2cc520cff2e0d96537
lvinhas <lenon.batista@virgo.inc>
2022-04-20 16:49:40 -0300

    added postinstall

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,23 +1,23 @@
-import ReactDOMServer from "react-dom/server";
-import { RemixServer } from "remix";
+import ReactDOMServer from 'react-dom/server'
+import {RemixServer} from 'remix'
 
 function handleRequest(
   request,
   responseStatusCode,
   responseHeaders,
-  remixContext
+  remixContext,
 ) {
   const markup = ReactDOMServer.renderToString(
-    <RemixServer context={remixContext} url={request.url} />
-  );
+    <RemixServer context={remixContext} url={request.url} />,
+  )
 
   return new Response(`<!DOCTYPE html>${markup}`, {
     status: responseStatusCode,
     headers: {
       ...Object.fromEntries(responseHeaders),
-      "Content-Type": "text/html",
+      'Content-Type': 'text/html',
     },
-  });
+  })
 }
 
-export default handleRequest;
+export default handleRequest
